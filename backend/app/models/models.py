98dc17068c0009361c9602a79cade11b85d3ce7e
Matteo Sandrin <sandrin98@gmail.com>
2026-01-02 15:34:48 -0500

    Format

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
 from app.database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     email = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    created_at = Column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -10,6 +10,7 @@ router = APIRouter(
     tags=["users"],
 )
 
+
 class GetUserResponse(BaseModel):
     id: int
     username: str
@@ -19,7 +20,11 @@ class GetUserResponse(BaseModel):
 @router.get("/")
 async def get_users(db: Session = Depends(get_db)) -> list[GetUserResponse]:
     users = db.query(User).all()
-    return [GetUserResponse(id=user.id, username=user.username, email=user.email) for user in users]
+    return [
+        GetUserResponse(id=user.id, username=user.username, email=user.email)
+        for user in users
+    ]
+
 
 @router.get("/{user_id}")
 async def get_user(user_id: int, db: Session = Depends(get_db)) -> GetUserResponse:
@@ -28,26 +33,35 @@ async def get_user(user_id: int, db: Session = Depends(get_db)) -> GetUserRespon
         raise HTTPException(status_code=404, detail="User not found")
     return GetUserResponse(id=user.id, username=user.username, email=user.email)
 
+
 class CreateUserRequest(BaseModel):
     username: str
     email: str
 
+
 class CreateUserResponse(BaseModel):
     id: int
 
+
 @router.post("/")
-async def create_user(body: CreateUserRequest, db: Session = Depends(get_db)) -> CreateUserResponse:
+async def create_user(
+    body: CreateUserRequest, db: Session = Depends(get_db)
+) -> CreateUserResponse:
     user = User(username=body.username, email=body.email)
     db.add(user)
     db.commit()
     db.refresh(user)
     return CreateUserResponse(id=user.id)
 
+
 class DeleteUserResponse(BaseModel):
     message: str
 
+
 @router.delete("/{user_id}")
-async def delete_user(user_id: int, db: Session = Depends(get_db)) -> DeleteUserResponse:
+async def delete_user(
+    user_id: int, db: Session = Depends(get_db)
+) -> DeleteUserResponse:
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
